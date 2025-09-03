b83de48c1230406e07e9c2b520d085bca9f28336
Zhen Yang <markt0815@icloud.com>
2025-09-02 22:21:44 -0400

    fix

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,5 @@
 # 这里的 outputs 是给 github action 用的
-output "bucket_name"                 { value = module.frontend_static_site.bucket_name }
+output "bucket_name"                 { value = module.frontend_static_site.frontend_bucket_name }
 output "cloudfront_domain_name"      { value = module.frontend_static_site.cloudfront_domain_name }
 output "cloudfront_distribution_id"  { value = module.frontend_static_site.cloudfront_distribution_id }
 output "github_actions_role_arn"     { value = module.frontend_static_site.github_actions_role_arn }
